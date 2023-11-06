5b68669964f69ddcb16e5b00a536b181730088af
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2023-11-06 15:49:17 -0300

    Adicionando tela de feedback no final do quiz

--- src/pages/DetailQuestion/index.jsx
+++ src/pages/DetailQuestion/index.jsx
@@ -8,6 +8,8 @@ export default function DetailQuestion() {
 
     const [indexAtual, setIndexAtual] = useState(0);
     const [acertos, setAcertos] = useState(0);
+    const [finalizado, setFinalizado] = useState(false);
+    const [feedback, setFeedback] = useState([]);
     const navigate = useNavigate();
     let perguntas= [];
 
@@ -24,32 +26,67 @@ export default function DetailQuestion() {
      
         if (e.target.getAttribute("data-correct") === "true") {
             setAcertos(acertos + 1);
+            setFeedback([...feedback, true]);
+        } else {
+            setFeedback([...feedback, false]);
         }
         
         if (indexAtual < perguntas.length - 1) {
             setIndexAtual(indexAtual + 1); 
         } else {
-            navigate('/');
+            setFinalizado(true);
+
         }
-
-
     }
 
+    function voltar() {
+        navigate('/')
+    }
+
+    function reiniciar() {
+        setFeedback([]);
+        setAcertos(0);
+        setIndexAtual(0);
+        setFinalizado(false);
+    }
 
     return (
         <main>
-            <div className="content">
-                <span className="tag"> {perguntas[indexAtual].tag} </span>
-                <span className="spnQtd"> {indexAtual+1}/{perguntas.length} </span>
-                <span className="question"> {perguntas[indexAtual].question} </span>
-                <div className="answers">
-                    {perguntas[indexAtual].answers.map((tema, index) => (
+            { finalizado ?  (
+                <div className="finish">
+                    <span> Você acertou {acertos} de 4 </span>
+                    <div className="feedback"> 
+                        {perguntas.map((tema, index) => {
+                            if(feedback[index] === true) {
+                                return (
+                                    <p key={index} className="green"> {index + 1}. {tema.question} </p>
+                                );
+                            } else {
+                                return (
+                                    <p key={index} className="red"> {index + 1}. {tema.question} </p>
+                                );
+                            }
+                            
+                        })}
+                    </div>
+                    <button onClick={reiniciar}>Reiniciar</button>
+                    <button className="voltar" onClick={voltar}> Voltar Menu </button>
+                </div>
+            ) : (
+                <div className="content">
+                    <span className="tag"> {perguntas[indexAtual].tag} </span>
+                    <span className="spnQtd"> {indexAtual+1}/{perguntas.length} </span>
+                    <span className="question"> {perguntas[indexAtual].question} </span>
+                    <div className="answers">
+                        {perguntas[indexAtual].answers.map((tema, index) => (
                         <button key={index} data-correct={`${tema.correct}`} onClick={(e) => nextQuestion(e)} > 
                             {tema.option} 
                         </button>
-                    ))}
-                </div>
-            </div>  
+                        ))}
+                    </div>
+                </div>     
+            )}
+
         </main>
     )
 
