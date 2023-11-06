a8a00a63ae67089bab965815b22d8a43306777cf
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2023-11-06 16:53:04 -0300

    Adicionando mais estilos e um footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Footer from './components/Footer';
 import  Header  from './components/Header';
 
 import { RouteList } from './routes';
@@ -8,6 +9,7 @@ function App() {
     <>
       <Header/>
       <RouteList/>
+      <Footer/>
     </>
   )
 }
---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,12 @@
+import './footer.css';
+
+export default function Footer() {
+  
+    return (
+    <footer className='footer'>
+        <small> © Desenvolvido por Luis Braga Dev </small>
+    </footer>
+  )
+  
+}
+
