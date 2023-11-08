8eea9ea2d1e4fe9d84c3fd071ecf0a0aa70e4c83
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2023-11-08 15:01:50 -0300

    Adicionando sweetAlert e feedback personalizado

--- src/pages/DetailQuestion/index.jsx
+++ src/pages/DetailQuestion/index.jsx
@@ -1,5 +1,7 @@
-import { useState } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
+import Swal from 'sweetalert2';
 
 import questions from '../../data/questions';
 import './detail.css';
@@ -8,11 +10,15 @@ export default function DetailQuestion() {
 
     const [indexAtual, setIndexAtual] = useState(0);
     const [acertos, setAcertos] = useState(0);
+    const [url, setUrl] = useState('');
     const [finalizado, setFinalizado] = useState(false);
     const [feedback, setFeedback] = useState([]);
+    const detail = useParams();
     const navigate = useNavigate();
+
     let perguntas= [];
 
+    const temasUnicos = [...new Set(questions.map(question => question.tag))];
     const coresTema = {
         '0': '#9a5833',
         '1': '#F7C942',
@@ -24,36 +30,49 @@ export default function DetailQuestion() {
         '7': '#009000',
         '8': '#666666',
         '9': '#daa520',
-        '10': '#820ad1',
+        '10': '#800080',
         '11': '#ff7fff',
     }
     
-    
-
-    const temasUnicos = [...new Set(questions.map(question => question.tag))];
-    const detail = useParams();
-
     for (let i = 0; i < questions.length; i++) {
         if(questions[i].tag === temasUnicos[detail.tag]) {
           perguntas.push(questions[i]);
         }
     }
 
+    useEffect( () => {
+        getCustomFeedback();
+        
+        if(finalizado) {
+            Swal.fire({
+                title: 'Jogo Finalizado!',
+                text: `Você acertou ${acertos} de ${perguntas.length}`,
+                imageUrl: `${url}`,
+                imageWidth: 400,
+                imageHeight: 200,
+                imageAlt: 'Custom image',
+                confirmButtonText: 'Ver feedback',
+                confirmButtonColor: 'green'
+              })
+        }
+
+    }, [acertos, url, finalizado])
+    
     function nextQuestion(e) {
      
         if (e.target.getAttribute("data-correct") === "true") {
-            setAcertos(acertos + 1);
+            setAcertos(acertos => acertos + 1);
             setFeedback([...feedback, true]);
         } else {
             setFeedback([...feedback, false]);
         }
         
         if (indexAtual < perguntas.length - 1) {
-            setIndexAtual(indexAtual + 1); 
+            setIndexAtual(indexAtual + 1);
         } else {
             setFinalizado(true);
-
         }
+        
     }
 
     function voltar() {
@@ -65,6 +84,20 @@ export default function DetailQuestion() {
         setAcertos(0);
         setIndexAtual(0);
         setFinalizado(false);
+    }
+
+    const getCustomFeedback = () => {
+        if(acertos === 0) {
+            setUrl('https://www.maxieduca.com.br/blog/wp-content/uploads/2017/10/Nota-Zero.jpg');
+          } else if(acertos === 1) {
+            setUrl('https://extra.globo.com/incoming/25209387-3f4-847/w640h360-PROP/meme-chloe.png');
+          } else if(acertos === 2) {
+            setUrl('https://pm1.aminoapps.com/6389/9c28e4faffa7fc8556b19863a3f2bbab507a1cab_hq.jpg');
+          } else if(acertos === 3) {
+            setUrl('https://ichef.bbci.co.uk/news/640/amz/worldservice/live/assets/images/2015/04/15/150415162940_success_kid_624x351_arquivopessoal.jpg');
+          } else if(acertos === 4) {
+            setUrl('https://media.tenor.com/53mmUaqb1b0AAAAC/copa-torcedor.gif');
+          }
     }
 
     return (
